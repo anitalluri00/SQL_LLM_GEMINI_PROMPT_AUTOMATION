4e09e610562c216eb555139b0e9fc01e6c086a42
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2025-07-01 17:47:18 +0530

    Create SQL_LLM_GEMINI_PROMPT.sh

---
+++ SQL_LLM_GEMINI_PROMPT.sh
@@ -0,0 +1,7 @@
+sudo yum install git docker -y
+systemctl start docker
+systemctl status docker
+git clone https://github.com/anitalluri00/SQL_LLM_GEMINI_PROMPT.git
+cd SQL_LLM_GEMINI_PROMPT
+docker build -t my-streamlit-app .
+docker run -p 8501:8501 my-streamlit-app
